5244c2ae6d78f1b88a297ac39dc75e0a8c2d39d4
notianwei <iwei2005@hotmail.com>
2017-02-10 01:51:23 -0500

    Empty Functions added , Main was update to display GUI

---
+++ src/GUIcontroller.java
@@ -0,0 +1,34 @@
+/**
+ * Created by 100628824 on 2/10/2017.
+ */
+public class GUIcontroller {
+    public void buybtnclick(){
+
+    }
+    public void redeem3btnclick(){
+
+    }
+
+    public void redeem5btnclick(){
+
+    }
+    public void redeem10btnclick(){
+
+    }
+
+    public void updatename1(){
+
+    }
+    public void updatename2(){
+
+    }
+    public void updatename3(){
+
+    }
+    public void updatename4(){
+
+    }
+    public void updatenam5(){
+
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,11 +1,26 @@
 /**
  *
  */
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args){
-        System.out.println("hello world");
 
-        // TO DO: call GUI
+public class Main extends Application {
+
+    @Override
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Tribev1.fxml"));
+        primaryStage.setTitle("Tribev1");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
